a37bed515d273159e09008eb6ba4556c3d1827c0
OscarTASC6 <oscart5292@rfwagner.org>
2019-07-15 14:02:21 -0400

    changes

--- sketch.js
+++ sketch.js
@@ -1,27 +1,40 @@
-function setup(){
-    createCanvas(500,500);
-    background(250,0,0);
+let shape;
+
+
+function setup() {
+    createCanvas(500, 500);
+    background(250, 0, 0);
+    var button = createButton("Reset");
+    button.position(510,0);
+    button.mousePressed(setup);
+    var re = createButton("Rectangle");
+    re.position(590,50);
+    re.mousePressed(changeToRectangle);
+    var cir = createButton("Circle");
+    cir.position();
+    cir.mousePressed(changetoCircle);
 }
 
-function mouseClicked(){
-    rect(mouseX, mouseY, random(50, 100), random(50, 100));
-}
-
-function mouseDragged(){
-    rect(mouseX, mouseY, random(50, 100), random(50, 100));
-}
-
-function draw(){
+function draw() {
     rect(125, 125, 50, 50);
     fill(124, 168, 192);
 
     circle(325, 325, 100);
     fill(0, 0, 134);
-    if (mouseIsPressed){
-        if (mouseButton === Left){
-            clear()
-        }
-    }
 }
 
+function changeToRectangle(){
+    shape = "rectangle";
+}
 
+function changetoCircle(){
+    shape = "circle";
+}
+
+function mouseDragged(){
+    if (shape === "rectangle"){
+        rect(mouseX, mouseY, 50, 50)
+    } else if (shape === "circle") {
+        circle(mouseX, mouseY, 50)
+    }
+}
